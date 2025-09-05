5e583273a94e4f1882ca646cdb4e510c8117c404
guptavika <aradhyy076@gmail.com>
2025-09-05 13:21:46 +0530

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,10 +3,4 @@ const express = require("express")
 const router = express.Router();
 
 
-router.get("/",(req,res) =>{
-    res.render("products")
-})
-router.post("/create",(req,res)=>{
-    console.log(req.body)
-})
 module.exports = router;
